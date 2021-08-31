b11894c9771f5f840eec9442dba978f4acc3fd39
KSSBro <shashwatdeadcliff@gmail.com>
2021-09-01 02:01:26 +0530

    update: program: src/entrypoint.rs: Processor::process()

--- program/src/entrypoint.rs
+++ program/src/entrypoint.rs
@@ -2,6 +2,8 @@ use solana_program::{
     account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, msg, pubkey::Pubkey,
 };
 
+use crate::processor::Processor;
+
 entrypoint!(process_instruction);
 fn process_instruction(
     program_id: &Pubkey,
@@ -14,5 +16,6 @@ fn process_instruction(
         accounts.len(),
         instruction_data
     );
-    Ok(())
+    
+    Processor::process(program_id, accounts, instruction_data)
 }

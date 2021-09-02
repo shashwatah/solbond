8983ca180fa809ecdec0902e5c92dd8e2210e471
KSSBro <shashwatdeadcliff@gmail.com>
2021-09-02 23:46:30 +0530

    add: client: files, index page design

---
+++ client/public/js/index.js
@@ -0,0 +1,15 @@
+VANTA.FOG({
+    el: "#index-main-container",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    highlightColor: 0x9ec1eb,
+    midtoneColor: 0xbd87b4,
+    lowlightColor: 0xea8083,
+    baseColor: 0xeeedee,
+    blurFactor: 0.7,
+    speed: 1.50,
+    zoom: 1.4
+});

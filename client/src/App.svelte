fd683e8f73b7666f771cf1d5947ef09f227379b7
KSSBro <shashwatdeadcliff@gmail.com>
2021-09-04 08:26:53 +0530

    update: client: src/App.svelte: added routing with page.js

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,37 +1,15 @@
 <script>
-  import { onMount } from "svelte";
-  import Navbar from "./Navbar.svelte";
-  import Footer from "./Footer.svelte";
-  import FormContainer from "./FormContainer.svelte";
+  import router from "page";
+  import Home from "./routes/Home.svelte";
+  import Certificate from "./routes/Certificate.svelte";
 
-  onMount(() => {
-    VANTA.FOG({
-      el: "#main-container",
-      mouseControls: true,
-      touchControls: true,
-      gyroControls: false,
-      minHeight: 200.0,
-      minWidth: 200.0,
-      highlightColor: 0x9ec1eb,
-      midtoneColor: 0xbd87b4,
-      lowlightColor: 0xea8083,
-      baseColor: 0xeeedee,
-      blurFactor: 0.7,
-      speed: 1.5,
-      zoom: 1.4,
-    });
-  });
+  let page;
+  let params;
 
-//   let mainPage = true;
+  router('/', () => (page = Home));
+  router('/cert', () => (page = Certificate));
+
+  router.start();
 </script>
 
-<div id="main-container">
-  <Navbar />
-
-  <FormContainer />
-
-  <Footer />
-</div>
-
-<style>
-</style>
+<svelte:component this="{page}" params="{params}" />

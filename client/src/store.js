003d289381ff288b9f7b35a3b80b9789b0454f3e
KSSBro <shashwatdeadcliff@gmail.com>
2021-09-05 14:32:55 +0530

    update: client: store: activeForm

--- client/src/store.js
+++ client/src/store.js
@@ -1,5 +1,11 @@
 import { readable, writable } from 'svelte/store';
 
+export const activeForm = writable("main");
+
 export const wallet = writable({});
 
 export const walletConnected = writable(false);
+
+export const registerData = writable({});
+
+export const validateData = writable({});

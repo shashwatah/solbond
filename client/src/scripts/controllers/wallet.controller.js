cfc8992e48df8cf2c903b5dd0e124cb13947433c
KSSBro <shashwatdeadcliff@gmail.com>
2021-09-14 23:33:59 +0530

    client: add: src/scripts/controllers: wallet.controller

---
+++ client/src/scripts/controllers/wallet.controller.js
@@ -0,0 +1,38 @@
+import Wallet from '@project-serum/sol-wallet-adapter';
+
+import { get } from 'svelte/store';
+import { wallet, walletConnected, activeForm, walletBtnValue } from './../../store.js';
+
+export const toggleWallet = async (providerUrl = "https://www.sollet.io") => {
+    if (!get(walletConnected)) {
+        initWallet(providerUrl);
+        await get(wallet).connect();
+    } else {
+        await get(wallet).disconnect();
+    }
+}
+
+const initWallet = (providerUrl) => {
+    wallet.set(new Wallet(providerUrl));
+
+    wallet.update(wallet => wallet.on("connect", handleWalletConnect));
+
+    wallet.update(wallet => wallet.on("disconnect", handleWalletDisconnect));
+}
+
+const handleWalletConnect = async (pubKey) => {
+    walletConnected.set(true);
+
+    let pubKeyString = await pubKey.toBase58();
+    walletBtnValue.set(pubKeyString.substring(0, 4) + '...' + pubKeyString.substring(pubKeyString.length - 4));
+    
+    console.log(`Wallet Connected, PubKey: ${pubKeyString}`);
+}
+
+const handleWalletDisconnect =  () => {
+    walletConnected.set(false);
+    walletBtnValue.set("Connect Wallet");
+    activeForm.set("main");
+
+    console.log("Wallet Disconnected");
+}   

7cb296062dd79151e48e2a5e1c6722007dcbf6f8
KSSBro <shashwatdeadcliff@gmail.com>
2021-09-21 03:01:53 +0530

    client: rename: scripts/utils: general.interface > general.interfaces

---
+++ client/src/scripts/utils/general.interfaces.ts
@@ -0,0 +1,15 @@
+export interface GenericData {
+  [key: string]: string;
+}
+
+export interface RegisterData extends GenericData {
+  name: string;
+  spouseName: string;
+  spousePubkeyString: string;
+  soulColor: string;
+}
+
+export interface ValidateData extends GenericData {
+  solbondPubkeyString: string;
+  soulColor: string;
+}

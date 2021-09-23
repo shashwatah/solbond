b6ebb4cc7234fad38760445bec3fe51152aa9e79
KSSBro <shashwatdeadcliff@gmail.com>
2021-09-24 02:47:55 +0530

    client: update: error.controller.js

--- client/src/scripts/controllers/error.controller.js
+++ client/src/scripts/controllers/error.controller.js
@@ -6,6 +6,9 @@ const errors = {
     BadPubkey: { message: 'Public key does not match criteria', type: 'warning' },
     DuplicateKey: { message: 'Duplicate key entered', type: 'warning' },
     BadColor: { message: "There's something wrong with the color input", type: 'warning' },
+    TransactionError: { message: 'Transaction resulted in an error', type: 'error'},
+    RegisterError: { message: 'Something went wrong while registering the solbond', type: 'error'},
+    ValidateError: { message: 'Something went wrong while validating the solbond', type: 'error'}
 };
 
 export const generateError = (type) => {
@@ -15,8 +18,9 @@ export const generateError = (type) => {
 };
 
 export const handleError = (err) => {
+    if (!err.name in errors || errors[err.name].type === "error") console.error(err);
+
     if (err.name in errors) return snackbarController(errors[err.name].type, err.message);
 
-    console.error(err);
     snackbarController(errors['GenericError'].type, errors['GenericError'].message);
 };

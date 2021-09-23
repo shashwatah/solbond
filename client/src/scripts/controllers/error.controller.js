5be4ad16c9d23c96b12caa9183b7e109196c549e
KSSBro <shashwatdeadcliff@gmail.com>
2021-09-24 00:55:51 +0530

    client: update: scripts/controller: error.controller

--- client/src/scripts/controllers/error.controller.js
+++ client/src/scripts/controllers/error.controller.js
@@ -1,10 +1,17 @@
+import { snackbarController } from "./snackbar.controller";
+
 const errors = {
     BadName: { message: 'Name does not match criteria', type: 'warning' },
-    BadPubkey: { message: "Spouse's Public Key does not match criteria", type: 'warning' },
+    BadPubkey: { message: "Public key does not match criteria", type: 'warning' },
     DuplicateKey: { message: 'Duplicate key entered', type: 'warning' },
     BadColor: { message: "There's something wrong with the color input", type: 'warning' },
 }
 
-export const generateError = (type, subtype) => {}
+export const generateError = (type) => {
+    let err = new Error();
+    err.name = type,
+    err.message = errors[type].message;
+    return err;
+}
 
-export const handleError = () => {}
+export const handleError = (err) => snackbarController(errors[err.name].type, err.message);

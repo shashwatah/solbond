d949d9fde4da8d91663782c93333c17ee78ad3d1
KSSBro <shashwatdeadcliff@gmail.com>
2021-09-24 02:47:30 +0530

    client: update: snackbar.controller: new types

--- client/src/scripts/controllers/snackbar.controller.js
+++ client/src/scripts/controllers/snackbar.controller.js
@@ -1,16 +1,18 @@
+let fileData = '';
+
 const typeSpecificData = {
     standard: {
-        backgroundColor: 'rgb(13, 7, 51)',
+        backgroundColor: 'rgb(13, 7, 5)',
         duration: 3000,
         showAction: false,
     },
     success: {
-        backgroundColor: 'rgba(0, 255, 0, 0.5',
+        backgroundColor: 'rgba(0, 255, 0, 0.5)',
         duration: 3000,
         showAction: false,
     },
     error: {
-        backgroundColor: 'rgba(255, 0, 0, 0.5',
+        backgroundColor: 'rgba(255, 0, 0, 0.5)',
         duration: 3000,
         showAction: false,
     },
@@ -19,12 +21,40 @@ const typeSpecificData = {
         duration: 3000,
         showAction: false,
     },
+    loading: {
+        backgroundColor: 'rgba(238, 210, 2, 0.5)',
+        duration: 30000,
+        showAction: false,
+    },
+    'success-register': {
+        backgroundColor: 'rgba(0, 255, 0, 0.5)',
+        duration: 1800000,
+        showAction: true,
+        actionText: 'SAVE',
+        actionTextColor: 'rgba(13, 7, 51, 0.7)',
+        onActionClick: function (ele) {
+            const a = document.createElement('a');
+            const file = new Blob([fileData], { type: 'text/plain' });
+
+            a.href = URL.createObjectURL(file);
+            a.download = 'solbond_data.txt';
+            a.click();
+
+            URL.revokeObjectURL(a.href);
+
+            ele.style.opacity = 0;
+
+            snackbarController("success", "Solbond data was saved!")
+        },
+    },
 };
 
-export const snackbarController = (type, text) => {
+export const snackbarController = (type, text, data = null) => {
     const standardConfig = { text, customClass: 'snackbar' };
 
     const snackbarConfig = Object.assign(standardConfig, typeSpecificData[type]);
+
+    fileData = data !== null ? data : fileData;
 
     Snackbar.show(snackbarConfig);
 };
